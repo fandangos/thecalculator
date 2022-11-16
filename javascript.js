7c145086770f2bae7707f8dc3bda64a2a2c9ee8b
Vitor DallAcqua <veggav@gmail.com>
2022-11-16 13:35:48 -0300

    Add touch event

--- javascript.js
+++ javascript.js
@@ -314,7 +314,19 @@ buttons.forEach(btn => {
 })
 
 buttons.forEach(btn => {
+    btn.addEventListener('touchstart', event => {
+        btn.style.transform = 'scale(0.9)';
+    })
+})
+
+buttons.forEach(btn => {
     btn.addEventListener('mouseup', event => {
+        btn.style.transform = 'none';
+    })
+})
+
+buttons.forEach(btn => {
+    btn.addEventListener('touchend', event => {
         btn.style.transform = 'none';
     })
 })
